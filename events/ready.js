e015df85a955523a344c54179d48cbbf1b788e4a
Colin <colin@cpdev.me>
2020-03-27 19:59:10 -0500

    Basic bot created

---
+++ events/ready.js
@@ -0,0 +1,6 @@
+const chainMessageAction = require('../eventActions/checkChainMessage');
+
+module.exports = (client) => {
+	chainMessageAction.loadChainMessageTracker(client);
+	console.log(`Running on ${client.channels.size} channels on ${client.guilds.size} servers.`);
+};
